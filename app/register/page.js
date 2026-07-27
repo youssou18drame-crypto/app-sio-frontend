bb1e0ce5e350a88329327b26b16104fa6112aaa8
El hadji youssou Drame <youssou18drame@gmail.com>
2026-07-27 13:44:38 +0200

    Fix import path in register page

--- app/register/page.js
+++ app/register/page.js
@@ -1,6 +1,6 @@
 'use client'
 import { useState } from 'react'
-import { Register } from '@/services/register'
+import { Register } from '../../services/register'
 
 export default function RegisterPage() {
   const [email, setEmail] = useState('')
