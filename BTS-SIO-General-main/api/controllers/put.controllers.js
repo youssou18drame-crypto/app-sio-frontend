87df1931c71fc3d1f1034d097d42e47770956e78
El hadji youssou Drame <youssou18drame@gmail.com>
2026-07-27 14:15:31 +0200

    Forcer le bon chemin relatif register

--- BTS-SIO-General-main/api/controllers/put.controllers.js
+++
@@ -1,7 +0,0 @@
-
-
-
-
-module.exports = (req, res) => {
-  res.send('/autoriser/avis/:id')
-}  
--- BTS-SIO-General-main/api/middleware/get.middleware.reviews.js
+++
@@ -1,7 +0,0 @@
-
-
-module.exports = (req, res,next) => {
-    console.log('Middleware exécuté avant le contrôleur')
-    next()
-}
-
